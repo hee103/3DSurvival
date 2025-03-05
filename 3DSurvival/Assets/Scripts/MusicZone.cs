da4529302c834419188e5d4e739f0d05c0426daa
hee103 <gmlgml3213@gmail.com>
2025-03-05 18:08:16 +0900

    [Feat] 걷는 소리, musicZone 구현

--- 3DSurvival/Assets/Scripts/MusicZone.cs
+++ 3DSurvival/Assets/Scripts/MusicZone.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class MusicZone : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public AudioSource audioSource;
+    public float fadeTime;
+    public float maxVolume;
+    private float targetVolume;
+
+    private void Start()
     {
-        
+        targetVolume = 0.0f;
+        audioSource = GetComponent<AudioSource>();
+        audioSource.volume = targetVolume;
+        audioSource.Play();
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (!Mathf.Approximately(audioSource.volume, targetVolume))
+        {
+            audioSource.volume = Mathf.MoveTowards(audioSource.volume, targetVolume, (maxVolume / fadeTime) * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            targetVolume = maxVolume;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            targetVolume = 0.0f;
+        }
     }
 }
+
